f0bdc57f239481176a04792072449ad5c8cfa0f3
cradu <cradu@localhost>
2010-11-15 16:10:51 +0200

    - generated random mappings for all APCGs; - formated the XML files.

--- src/ro/ulbsibiu/acaps/mapper/random/RandomMapper.java
+++ src/ro/ulbsibiu/acaps/mapper/random/RandomMapper.java
@@ -38,16 +38,16 @@ public class RandomMapper implements Mapper {
 	 * Logger for this class
 	 */
 	private static final Logger logger = Logger.getLogger(RandomMapper.class);
-	
+
 	/** the APCG XML file */
 	private File apcgFile;
-	
+
 	/** a list with the IDs of all of the NoC nodes */
 	private List<String> nodeIds;
-	
+
 	/** holds the mapping (cores are identified by IDs) */
 	private Map<String, String> coresToNodes;
-	
+
 	/**
 	 * Constructor
 	 * 
@@ -57,15 +57,18 @@ public class RandomMapper implements Mapper {
 	 *            a list with the IDs of all of the NoC nodes (cannot be empty)
 	 */
 	public RandomMapper(String apcgFilePath, List<String> nodeIds) {
-		logger.assertLog(apcgFilePath != null && apcgFilePath.length() > 0, "Unspecified APCG");
-		logger.assertLog(apcgFilePath.endsWith(".xml"), "The APCG must be an XML file");
-		logger.assertLog(nodeIds != null && nodeIds.size() > 0, "The list of NoC nodes is not specified");
-		
+		logger.assertLog(apcgFilePath != null && apcgFilePath.length() > 0,
+				"Unspecified APCG");
+		logger.assertLog(apcgFilePath.endsWith(".xml"),
+				"The APCG must be an XML file");
+		logger.assertLog(nodeIds != null && nodeIds.size() > 0,
+				"The list of NoC nodes is not specified");
+
 		logger.info("Working with the APCG from the XML " + apcgFilePath);
-		
+
 		apcgFile = new File(apcgFilePath);
 		logger.assertLog(apcgFile.isFile(), "The APCG is not a file");
-		
+
 		this.nodeIds = nodeIds;
 	}
 
@@ -81,13 +84,14 @@ public class RandomMapper implements Mapper {
 		if (logger.isDebugEnabled()) {
 			logger.debug("Random mapping started");
 		}
-		
+
 		String mappingXml = null;
 
 		try {
 			String[] coreIds = getCoreIds();
 			if (coreIds.length > nodeIds.size()) {
-				throw new TooFewNocNodesException(coreIds.length, nodeIds.size());
+				throw new TooFewNocNodesException(coreIds.length,
+						nodeIds.size());
 			}
 			coresToNodes = new HashMap<String, String>(coreIds.length);
 			Random random = new Random();
@@ -97,11 +101,12 @@ public class RandomMapper implements Mapper {
 			for (int i = 0; i < coreIds.length; i++) {
 				int k = random.nextInt(tempNodeIds.size());
 				coresToNodes.put(coreIds[i], tempNodeIds.get(k));
-				logger.info("Core " + coreIds[i] + " is mapped to node " + tempNodeIds.get(k));
+				logger.info("Core " + coreIds[i] + " is mapped to node "
+						+ tempNodeIds.get(k));
 				tempNodeIds.remove(k);
 			}
 			mappingXml = generateMap();
-			
+
 		} catch (JAXBException e) {
 			logger.error("JAXB encountered an error", e);
 		}
@@ -109,7 +114,7 @@ public class RandomMapper implements Mapper {
 		if (logger.isDebugEnabled()) {
 			logger.debug("Random mapping finished");
 		}
-		
+
 		return mappingXml;
 	}
 
@@ -119,16 +124,17 @@ public class RandomMapper implements Mapper {
 		Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
 		JAXBElement<ApcgType> apcgXml = (JAXBElement<ApcgType>) unmarshaller
 				.unmarshal(apcgFile);
-		List<ro.ulbsibiu.acaps.ctg.xml.apcg.CoreType> cores = apcgXml.getValue().getCore();
-		
+		List<ro.ulbsibiu.acaps.ctg.xml.apcg.CoreType> cores = apcgXml
+				.getValue().getCore();
+
 		String[] coreIds = new String[cores.size()];
 		for (int i = 0; i < cores.size(); i++) {
 			coreIds[i] = cores.get(i).getId();
 		}
-		
+
 		return coreIds;
 	}
-	
+
 	private String getApcgId() throws JAXBException {
 		JAXBContext jaxbContext = JAXBContext
 				.newInstance("ro.ulbsibiu.acaps.ctg.xml.apcg");
@@ -145,7 +151,7 @@ public class RandomMapper implements Mapper {
 		if (logger.isDebugEnabled()) {
 			logger.debug("Generating an XML String with the mapping");
 		}
-		
+
 		ObjectFactory mappingFactory = new ObjectFactory();
 		MappingType mappingType = new MappingType();
 		String id = getApcgId();
@@ -163,32 +169,38 @@ public class RandomMapper implements Mapper {
 		JAXBContext jaxbContext = JAXBContext.newInstance(MappingType.class);
 		Marshaller marshaller = jaxbContext.createMarshaller();
 		StringWriter stringWriter = new StringWriter();
-		JAXBElement<MappingType> mapping = mappingFactory.createMapping(mappingType);
+		JAXBElement<MappingType> mapping = mappingFactory
+				.createMapping(mappingType);
 		marshaller.marshal(mapping, stringWriter);
 
 		return stringWriter.toString();
 	}
 
-	public static void main(String[] args) throws FileNotFoundException, TooFewNocNodesException {
+	public static void main(String[] args) throws FileNotFoundException,
+			TooFewNocNodesException {
 		String e3sBenchmark = "auto-indust-mocsyn.tgff";
-		String mappingId = "1";
-		String apcgId = "1";
-		String ctgId = "0";
-		
-		String path = "xml" + File.separator + "e3s" + File.separator
-				+ e3sBenchmark + File.separator;
-		List<String> nodeIds = new ArrayList<String>();
-		for (int i = 0; i < 16; i++) {
-			nodeIds.add(Integer.toString(i));
+
+		for (int i = 0; i < 2; i++) {
+			String mappingId = Integer.toString(i);
+			String apcgId = Integer.toString(i);
+			for (int j = 0; j < 4; j++) {
+				String ctgId = Integer.toString(j);
+				String path = "xml" + File.separator + "e3s" + File.separator
+						+ e3sBenchmark + File.separator;
+				List<String> nodeIds = new ArrayList<String>();
+				for (int k = 0; k < 16; k++) {
+					nodeIds.add(Integer.toString(k));
+				}
+				Mapper mapper = new RandomMapper(path + "ctg-" + ctgId
+						+ File.separator + "apcg-" + apcgId + ".xml", nodeIds);
+				String mappingXml = mapper.map();
+				PrintWriter pw = new PrintWriter(path + "ctg-" + ctgId
+						+ File.separator + "mapping-" + mappingId + ".xml");
+				logger.info("Saving the mapping XML file");
+				pw.write(mappingXml);
+				pw.close();
+			}
 		}
-		Mapper mapper = new RandomMapper(path + "ctg-" + ctgId
-				+ File.separator + "apcg-" + apcgId + ".xml", nodeIds);
-		String mappingXml = mapper.map();
-		PrintWriter pw = new PrintWriter(path + "ctg-" + ctgId + File.separator
-				+ "mapping-" + mappingId + ".xml");
-		logger.info("Saving the mapping XML file");
-		pw.write(mappingXml);
-		pw.close();
 	}
 
 }
