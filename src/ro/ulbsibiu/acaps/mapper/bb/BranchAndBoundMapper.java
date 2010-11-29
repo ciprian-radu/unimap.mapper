ad61f1e7872cb21218e7d870d6ea63d79ed99f39
cipi <cipi@localhost>
2010-11-29 16:36:44 +0200

    saving mapping runtime

--- src/ro/ulbsibiu/acaps/mapper/bb/BranchAndBoundMapper.java
+++ src/ro/ulbsibiu/acaps/mapper/bb/BranchAndBoundMapper.java
@@ -1183,6 +1183,7 @@ public class BranchAndBoundMapper implements Mapper {
 
 		MappingType mapping = new MappingType();
 		mapping.setId(MAPPER_ID);
+		mapping.setRuntime(new Double(end - start));
 		for (int i = 0; i < nodes.length; i++) {
 			if (!"-1".equals(nodes[i].getCore())) {
 				MapType map = new MapType();
