3b9bcee3ac20e3153da6032334401640aeb65387
cipi <cipi@localhost>
2010-12-20 13:28:25 +0200

    bug fix: the job name was incorrect

--- lava-schedule-program-n-times.sh
+++ lava-schedule-program-n-times.sh
@@ -5,5 +5,5 @@ echo ""
 echo "usage: ./lava-schedule-program-n-times.sh program n"
 echo "(where program is the file path to the program and n is the number of times the program will run)"
 for i in `seq 1 $2`; do
-	exec bsub -o job-%J.out.log -e job-%J.err.log -J $1-$2 $1
+	exec bsub -o job-%J.out.log -e job-%J.err.log -J $1-$i $1
 done
