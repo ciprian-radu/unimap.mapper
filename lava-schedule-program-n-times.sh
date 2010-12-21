8ff89e9c9da54c4b7dfa6b4e78e732d936c6637e
cradu <cradu@localhost>
2010-12-21 10:35:20 +0200

    the script is not simpler and better

--- lava-schedule-program-n-times.sh
+++ lava-schedule-program-n-times.sh
@@ -4,6 +4,12 @@ echo "This script uses the LAVA job scheduler to run a program multiple times."
 echo ""
 echo "usage: ./lava-schedule-program-n-times.sh program n"
 echo "(where program is the file path to the program and n is the number of times the program will run)"
-for i in `seq 1 $2`; do
-	exec bsub -o job-%J.out.log -e job-%J.err.log -J $1-$i $1 &
+
+for i in $(seq 1 $2)
+do
+	echo $i $2
+	bsub -o job-%J.out.log -e job-%J.err.log -J $1-$i $1
 done
+
+echo "Job $1 has been submitted $2 times."
+
