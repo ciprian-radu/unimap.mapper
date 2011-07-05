2d4774fb1d8dd2123a941da51f29220e524a9253
cradu <cradu@localhost>
2011-07-05 16:27:13 +0300

    wrong name...

--- map-app-zamolxe-eta-jmetal.sh
+++ map-app-zamolxe-eta-jmetal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-MAPPER="ro.ulbsibiu.acaps.mapper.ga.ea.EnergyAndTemperatureAwareJMetalEvolutionaryAlgorithmMapper"
+MAPPER="ro.ulbsibiu.acaps.mapper.ga.ea.EnergyAndTemperatureAwareJMetalMultiObjectiveEvolutionAlgorithm"
 export MAPPER=$MAPPER
 exec ./map-app-zamolxe.sh $@
